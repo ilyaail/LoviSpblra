46b1b17d361c865e5ca31c5f97d77926a52a2d0c
Ilya <ilyailysha.k@gmail.com>
2019-08-21 15:42:24 +0300

    Created first page
    First page

---
+++ js/common.js
@@ -0,0 +1,34 @@
+(function($) {
+$(document).ready(function(){
+
+	// Custom JS
+     $('select').niceSelect();	
+
+});
+    $(document).ready(function(){
+
+        $('.certificate_open').on('click', function(){
+            $('.sertificates_drop').fadeToggle('2800');
+        });
+       $(".collapse.show").each(function(){
+            $(this).prev(".card-header").find(".material-icons").text('remove');
+        });
+        
+         $(".collapse").on('show.bs.collapse', function(){
+            $(this).prev(".card-header").find(".material-icons").text('remove')
+        }).on('hide.bs.collapse', function(){
+            $(this).prev(".card-header").find(".material-icons").text('add');
+        });
+    });
+    $(function () {
+    $('select').on('change', function () {
+        $('.current').html(this.value);
+    }).change();
+
+    $('.current').on('click', function () {
+        $('select').focus();
+    });
+});
+})(jQuery);
+
+
